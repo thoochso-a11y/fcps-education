5d0b43705b1c23028e1b0435eeea1f26adc0c629
dogehub <thooch.so@gmail.com>
2026-06-27 12:34:11 -0400

    Update s_sw.js to reference repofcps CDN

--- s_sw.js
+++ s_sw.js
@@ -1,1 +1,4 @@
-navigator.userAgent.includes("Firefox")&&Object.defineProperty(globalThis,"crossOriginIsolated",{value:!0,writable:!1}),importScripts("z/b.js");const{ScramjetServiceWorker}=$scramjetLoadWorker(),scramjet=new ScramjetServiceWorker;self.addEventListener("fetch",e=>{try{if(!scramjet.route(e.request))return void e.respondWith(fetch(e.request));e.respondWith(scramjet.fetch(e.request))}catch(e){console.error("Service worker error:",e)}});
+navigator.userAgent.includes("Firefox")&&Object.defineProperty(globalThis,"crossOriginIsolated",{value:!0,writable:!1});
+importScripts("https://cdn.jsdelivr.net/gh/thoochso-a11y/repofcps/z/b.js");
+const{ScramjetServiceWorker}=$scramjetLoadWorker(),scramjet=new ScramjetServiceWorker;
+self.addEventListener("fetch",e=>{try{if(!scramjet.route(e.request))return void e.respondWith(fetch(e.request));e.respondWith(scramjet.fetch(e.request))}catch(e){console.error("Service worker error:",e)}});
