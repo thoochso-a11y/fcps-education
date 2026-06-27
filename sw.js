971e1b1e7e45180a4690755cbb475e1482606527
dogehub <thooch.so@gmail.com>
2026-06-27 12:33:52 -0400

    Update sw.js to reference repofcps CDN for large files

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-importScripts("q/c.js"),importScripts("q/e.js"),importScripts("q/d.js");const uv=new UVServiceWorker;async function handleRequest(e){return uv.route(e)?await uv.fetch(e):await fetch(e.request)}self.addEventListener("fetch",e=>e.respondWith(handleRequest(e))),self.addEventListener("message",e=>{"INIT_UV"===e.data.msg&&(self.UVServiceWorker=e.data.UVServiceWorker)});
+importScripts("https://cdn.jsdelivr.net/gh/thoochso-a11y/repofcps/q/c.js");
+importScripts("https://cdn.jsdelivr.net/gh/thoochso-a11y/repofcps/q/e.js");
+importScripts("https://cdn.jsdelivr.net/gh/thoochso-a11y/repofcps/q/d.js");
+const uv=new UVServiceWorker;
+async function handleRequest(e){return uv.route(e)?await uv.fetch(e):await fetch(e.request)}
+self.addEventListener("fetch",e=>e.respondWith(handleRequest(e)));
+self.addEventListener("message",e=>{"INIT_UV"===e.data.msg&&(self.UVServiceWorker=e.data.UVServiceWorker)});
